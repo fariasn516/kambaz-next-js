1c4291486d316dcc8649c71160143f19af104804
fariasn516 <145229665+fariasn516@users.noreply.github.com>
2025-10-03 22:28:17 -0400

    Add name to lab page

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -1,25 +1,36 @@
 import Link from "next/link";
+
 export default function Labs() {
   return (
     <div id="wd-labs">
       <h1>Labs</h1>
+      <p>Nancy Farias</p> 
+
       <ul>
         <li>
           <Link href="/Labs/Lab1" id="wd-lab1-link">
-            Lab 1: HTML Examples{" "}
+            Lab 1: HTML Examples
           </Link>
         </li>
         <li>
           <Link href="/Labs/Lab2" id="wd-lab2-link">
-            Lab 2: CSS Basics{" "}
+            Lab 2: CSS Basics
           </Link>
         </li>
         <li>
           <Link href="/Labs/Lab3" id="wd-lab3-link">
-            Lab 3: JavaScript Fundamentals{" "}
+            Lab 3: JavaScript Fundamentals
           </Link>
         </li>
       </ul>
+
+      <Link
+        href="https://github.com/fariasn516/kambaz-next-js/tree/a2"
+        id="wd-github"
+        target="_blank"
+      >
+        View GitHub Repository
+      </Link>
     </div>
   );
 }
