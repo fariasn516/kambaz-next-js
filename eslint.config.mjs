6700aad276443c251b675cd4aa16dd9f7a0d8cbd
fariasn516 <145229665+fariasn516@users.noreply.github.com>
2025-10-17 19:46:43 -0400

    Lint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,8 +20,12 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
     rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+      "prefer-const": "off",
+      "no-var": "off",
+
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
